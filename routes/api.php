0a5b777896fa4e74a712884f64fc6f6e6637e637
RaviBx <ravimuay@gmail.com>
2021-05-27 19:12:01 +0530

    Added upcoming task resources

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,66 @@
 <?php
 
+use App\Http\Resources\UpcomingTaskResource;
+use App\Models\Upcoming;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// ** UPCOMING TASK **//
+
+
+//Get all the upcoming task
+Route::get("/upcoming", function(){
+
+        $upcoming = Upcoming::all();
+
+        return UpcomingTaskResource::collection($upcoming);
+});
+
+
+//Add a new task
+
+Route::post('/upcoming', function (Request $request) {
+
+
+        return Upcoming::create([
+
+
+            'title' =>  $request->title,
+            'taskID' => $request->taskID,
+            'waiting' => $request->waiting
+
+        ]);    
+    
+});
+
+//Delete upcoming task
+
+Route::delete('/upcoming/{taskID}', function($taskID){
+
+
+            DB::table('upcomings')->where('taskID', $taskID)->delete();
+
+            return 204;
+
+});
+
+
+
+// ** Today Task ** //
+
+
+
+
+
+
+
+
